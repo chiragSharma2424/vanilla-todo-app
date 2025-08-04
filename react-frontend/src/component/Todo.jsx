5b91769663eaad8b3e19884bf62fc25612ce45c0
chirag <sharmachirag242004@gmail.com>
2025-08-04 15:37:27 +0530

    new

--- react-frontend/src/component/Todo.jsx
+++ react-frontend/src/component/Todo.jsx
@@ -51,9 +51,9 @@ const Todo = () => {
     }
 
   return (
-    <div className="max-w-xl mx-auto mt-10 p-6 bg-white shadow-lg rounded-2xl border border-purple-200 space-y-5">
+    <div className="max-w-xl mx-auto mt-30 p-6 bg-white shadow-lg rounded-2xl border border-purple-200 space-y-5">
       
-      <h2 className="text-3xl font-bold text-center text-purple-700"> 📝 My Todo List </h2>
+      <h2 className="text-3xl font-bold text-center text-purple-700"> 📝 My Tudo List </h2>
 
       <input type="text" placeholder="Enter Todo Title"className="w-full px-4 py-3 rounded-xl border border-purple-300 bg-purple-50 placeholder-purple-400 text-gray-800 focus:outline-none focus:ring-2 focus:ring-purple-400 transition duration-300"
       onChange={(e) => {
